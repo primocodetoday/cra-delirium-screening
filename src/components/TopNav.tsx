4862f6e8f4b4cee7c8fab907e7156c783319ff6e
primocodetoday <primocodetoday@gmail.com>
2020-12-03 22:53:33 +0100

    dev: TopNav, Main created

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,20 @@
+﻿import * as React from 'react'
+import {Navbar, Nav} from 'react-bootstrap'
+
+const TopNav = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Delirium Screening</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link className="ml-auto text-uppercase" href="#home">Home</Nav.Link>
+        <Nav.Link className="ml-auto text-uppercase" href="#link">Admin</Nav.Link>
+        <Nav.Link className="ml-auto " href="#home">Wyloguj</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+</Navbar>
+  )
+}
+
+export default TopNav
---
+++ src/views/Main.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import { Button, Form, Row,Col} from 'react-bootstrap'
+import * as dayjs from 'dayjs'
+
+const Main = () => {
+  const now = dayjs.default().format('YYYY-MM-DD HH:mm:ss')
+  console.log(now)
+
+  return (
+    <div>
+      <div className="my-3 text-center">Aktualna godzina: <span className="font-weight-bold">{now}</span></div>
+      <Row className="mt-4 justify-content-center">
+          <Button className="m-3">Skala DOSS</Button>
+          <Button className="m-3">Czynniki Ryzyka</Button>
+          <Button className="m-3">Wyniki leczenia</Button>
+        </Row>
+    </div>
+  );
+};
+
+export default Main;
